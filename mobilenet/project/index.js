a588c0161d023ce8b316cd1dbaaa1fb0177de09f
MasterCoder <sparlevi18@gmail.com>
2023-10-17 16:08:23 -0700

    JS Isues

--- mobilenet/project/index.js
+++ mobilenet/project/index.js
@@ -1,1 +1,30 @@
 const { VisionClient } = require('@google-cloud/vision');
+const p5 = require('node-p5');
+
+const client = new VisionClient();
+
+function setup() {
+	console.log('success');
+}
+
+function draw() {
+	identify("../../../Sound_controlled_aliens/aliens-01.png");
+}
+
+function identify(image) {
+	var objects = client.detectObjects(image, (err, results) => {
+		if (err) {
+			console.log(err);
+			return;
+		}
+
+		const objects = results[0].objectAnnotations;
+
+		console.log(objects);
+		return objects;
+	});
+	return objects;
+}
+
+p5.setup();
+p5.draw();
