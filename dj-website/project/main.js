f7115d0afee70f046cb8e2380d004c8c4a5799fd
MasterCoder <sparlevi18@gmail.com>
2023-12-17 11:48:37 -0800

    modified:   dj-website/project/main.js

--- dj-website/project/main.js
+++ dj-website/project/main.js
@@ -1,6 +1,8 @@
 function preload() {
 	song1 = loadSound("assets/Midnight-Escapade.mp3");
 	song2 = loadSound("assets/midnight-chase.mp3");
+	song2.pause();
+	song1.pause();
 }
 function setup() {
 	canvas = createCanvas(600, 500);
@@ -20,9 +22,16 @@ function draw() {
 }
 function gotPoses(results) {
 	if (results.length > 0) {
-		console.log(results);
-		if (results[0].pose.rightWrist.y < 250 && results[0].pose.rightWrist.confidence > 65) {
-			song = 
+		console.log(results, results[0].pose.rightWrist.y, results[0].pose.leftWrist.y);
+		if (results[0].pose.rightWrist.y < 250 && song2.isPaused()) {
+			song1.pause();
+			song2.play();
+		} else if (results[0].pose.leftWrist.y < 250 && song1.isPaused()) {
+			song2.pause();
+			song1.play();
+		} else {
+			song2.pause();
+			song1.pause();
 		}
 	}
 }
