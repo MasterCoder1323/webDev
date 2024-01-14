e9294ddf102526c2d0cf58a73521230fbb109bc2
MasterCoder <sparlevi18@gmail.com>
2024-01-14 07:24:06 -0800

    remove pointer

--- video-object-detector/main.js
+++ video-object-detector/main.js
@@ -1,6 +1,3 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256: 6c14e6acc3d6f81fcac965fa8924c9d6117914bd4cd8b2e01089be7f67e48c9f
-size 1036
 var model, objects, video;
 var statusVar = document.getElementById('status');
 
--- video-object-detector/project/main.js
+++ video-object-detector/project/main.js
@@ -1,6 +1,3 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256: efed63fe584afbef42f50bf84281f46420ea685f63e32ab94c6cc1e626233040
-size 1128
 var model, objects;
 var statusVar = document.getElementById('status');
 var finding = "cell phone";
