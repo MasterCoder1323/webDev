76e10d0a843afe2ece3ee85c10d97663f724b7b6
MasterCoder <sparlevi18@gmail.com>
2024-01-14 07:12:57 -0800

    some changes

--- video-object-detector/main.js
+++ video-object-detector/main.js
@@ -1,3 +1,49 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c14e6acc3d6f81fcac965fa8924c9d6117914bd4cd8b2e01089be7f67e48c9f
+oid sha256: 6c14e6acc3d6f81fcac965fa8924c9d6117914bd4cd8b2e01089be7f67e48c9f
 size 1036
+var model, objects, video;
+var statusVar = document.getElementById('status');
+
+function preload() {
+	video = createVideo(["assets/video.mp4", "assets/video.webm"]);
+}
+
+function setup() {
+	canvas = createCanvas(640, 480);
+	canvas.parent('canvas-holder');
+	model = ml5.objectDetector('cocossd', modelLoaded);
+	video.hide();
+}
+
+function draw() {
+	image(video, 0, 0, 640, 480);
+	model.detect(video, gotResult);
+	if (objects) {
+		objects.forEach(function (object) {
+			fill(255, 0, 0);
+			text(object.label + " " + (object.confidence * 100).toFixed(2) + "%", object.x + 15, object.y + 15);
+			noFill();
+			stroke(255, 0, 0);
+			rect(object.x, object.y, object.width, object.height);
+		});
+		document.getElementById("numO").innerHTML = objects.length;
+	}
+}
+
+function modelLoaded() {
+	console.log("Model Loaded: ", model);
+	statusVar.innerHTML = "Detecting Objects";
+	video.loop();
+	video.volume(0.5);
+}
+
+function gotResult(error, results) {
+	if (error) {
+		console.log(error);
+		alert(error);
+	}
+	console.log(results);
+	objects = results;
+}
+
+
--- video-object-detector/project/main.js
+++ video-object-detector/project/main.js
@@ -1,3 +1,54 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:efed63fe584afbef42f50bf84281f46420ea685f63e32ab94c6cc1e626233040
+oid sha256: efed63fe584afbef42f50bf84281f46420ea685f63e32ab94c6cc1e626233040
 size 1128
+var model, objects;
+var statusVar = document.getElementById('status');
+var finding = "cell phone";
+
+function setup() {
+	canvas = createCanvas(640, 480);
+	canvas.parent('canvas-holder');
+	model = ml5.objectDetector('cocossd', modelLoaded);
+	video = createCapture(VIDEO);
+	video.size(640, 480);
+	video.hide();
+}
+
+function draw() {
+	image(video, 0, 0, 640, 480);
+	model.detect(video, gotResult);
+	if (objects) {
+		found = false;
+		objects.forEach(function (object) {
+			if (object.label == finding) {
+				found = true;
+			}
+			fill(255, 0, 0);
+			text(object.label + " " + (object.confidence * 100).toFixed(2) + "%", object.x + 15, object.y + 15);
+			noFill();
+			stroke(255, 0, 0);
+			rect(object.x, object.y, object.width, object.height);
+		});
+		if (found) {
+			statusVar.innerHTML = "Found";
+		} else {
+			statusVar.innerHTML = "Not Found";
+		}
+	}
+}
+
+function modelLoaded() {
+	console.log("Model Loaded: ", model);
+}
+
+function gotResult(error, results) {
+	if (error) {
+		console.log(error);
+	}
+	console.log(results);
+	objects = results;
+}
+
+function changeObject() {
+	finding = document.getElementById("object-to-find").value;
+}
