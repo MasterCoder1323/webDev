507be4b5cd7d61307fe77f68e90cccd80ebbfd18
MasterCoder <sparlevi18@gmail.com>
2024-01-20 12:36:51 -0800

    mario posenet

--- game/posenet-mario/main.js
+++ game/posenet-mario/main.js
@@ -1,5 +1,10 @@
 function preload() {
 	world_start = loadSound("assets/sounds/world_start.wav");
+	mario.jump = loadSound("assets/sounds/jump.wav");
+	mario.coin = loadSound("assets/sounds/coin.wav");
+	mario.kick = loadSound("assets/sounds/kick.wav");
+	mario.die = loadSound("assets/sounds/mariodie.wav");
+	gameoversound = loadSound("assets/sounds/gameover.wav")
 	setSprites();
 	MarioAnimation();
 }
@@ -7,7 +12,28 @@ function preload() {
 function setup() {
 	canvas = createCanvas(1240, 336);
 	canvas.parent('canvas');
+
+	video = createCapture(VIDEO);
+	video.size(800, 400);
+	video.parent("game_consol");
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+
 	instializeInSetup(mario);
+}
+
+function modelLoaded() {
+	console.log("Model Has Been Loaded:", poseNet);
+}
+
+function gotPoses(error, results) {
+	if (error) {
+		console.log(error);
+	} else if (results.length > 0) {
+		console.log('Successful Results', results);
+		nose = results[0].pose.nose;
+	}
 }
 
 function draw() {
